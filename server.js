37b4fd61ae101e28eba48fde079c51cc92624410
kanhaiya chauhan <kanhaiyachauhan037@gmail.com>
2023-05-21 23:20:06 +0530

    olx

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const classifiedsRoutes = require('./routes/classRoute');
-
+const cors = require("cors");
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
 mongoose
